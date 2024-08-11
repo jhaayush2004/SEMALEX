6713a9134d37fe65ea2c0f8dd9a1eb93a2a9aa46
Ayush Shaurya Jha <164651751+jhaayush2004@users.noreply.github.com>
2024-08-11 21:30:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SEMALEX',
-    version='0.1.2',
+    version='1.1.0',
     packages=find_packages(),
     install_requires=[
         'nltk',
